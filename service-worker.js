ae9b9b57930f7db70ff3f56ed44ca53ddbf38a1e
Martin H. Bramwell <martinhbramwell@gmail.com>
2018-11-05 11:07:00 -0500

    Test (ghp): Push latest version of project offsppwa-vue: (v0.0.96)
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63dc18f8c8a02c2400e8541c1d42862d.js"
+  "/precache-manifest.ca796800d5a64c74446ad9c02a893996.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuesppwa4"});
